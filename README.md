22f80ee9e81db0ae67b404e4eeaf022ab456bf74
ktds <ktds@ktdsui-MacBookPro-4.local>
2023-03-31 16:46:42 +0900

    spring multi module tutorial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Creating a Multi Module Project - Spring tutorial
+https://spring.io/guides/gs/multi-module/
+
+projects
+
+└── library
+
+└── application
+
+library : non executable jar. library.  
+
+application : executable jar(Server) that uses the library above
---
+++ library/src/test/java/com/example/multimodule/service/MyServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.multimodule.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest("service.message=Hello")
+class MyServiceTest {
+
+    @Autowired
+    private MyService myService;
+
+    @Test
+    public void contextLoads(){
+        assertNotNull(myService.message());
+    }
+
+    @SpringBootApplication
+    static class TestConfiguration {
+    }
+
+}
